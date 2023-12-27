931fc8d28f9ad972bd0aa948497aa4b3e329e32f
Om334exe <134759580+Om334exe@users.noreply.github.com>
2023-12-27 15:24:05 +0530

    Add files via upload

---
+++ sections (2).py
@@ -0,0 +1,298 @@
+# sections.py
+
+section_list = [
+    "Definitions",
+    "Sense of expression once explained",
+    "Gender",
+    "Number",
+    "Man",
+    "Woman",
+    "Person",
+    "Public",
+    "[Omitted.]",
+    "Servant of Government",
+    "[Repealed.]",
+    "[Repealed.]",
+    "Government",
+    "India",
+    "Judge",
+    "Court of Justice",
+    "Public servant",
+    "Moveable property",
+    "Wrongful gain",
+    "Wrongful loss",
+    "Gaining wrongfully/ Losing wrongfully",
+    "Dishonestly",
+    "Fraudulently",
+    "Reason to believe",
+    "Property in possession of wife, clerk or servant",
+    "Counterfeit",
+    "Document",
+    "Electronic record",
+    "Valuable security",
+    "A will",
+    "Words referring to acts include illegal omissions",
+    "Act",
+    "Omission",
+    "Acts done by several persons in furtherance of common intention",
+    "When such an act is criminal by reason of its being done with a criminal knowledge or intention",
+    "Effect caused partly by act and partly by omission",
+    "Co-operation by doing one of several acts constituting an offence",
+    "Persons concerned in criminal act may be guilty of different offences",
+    "Voluntarily",
+    "Offence",
+    "Special law",
+    "Local law",
+    "Illegal",
+    "Legally bound to do",
+    "Injury",
+    "Life",
+    "Death",
+    "Animal",
+    "Vessel",
+    "Year",
+    "Month",
+    "Section",
+    "Oath",
+    "Good faith",
+    "Harbour-",
+    "CHAPTER III OF P UNISHMENTS",
+    "Punishments",
+    "Construction of reference to transportation",
+    "Commutation of sentence of death",
+    "Commutation of sentence of imprisonment for life",
+    "Definition of \"appropriate Government\"",
+    "[Repealed.]",
+    "Fractions of terms of punishment",
+    "[Repealed.]",
+    "[Repealed.]",
+    "Sentence may be (in certain cases of imprisonment) wholly or partly rigorous of simple",
+    "[Repealed.]",
+    "[Repealed.]",
+    "Amount of fine",
+    "Sentence of imprisonment for non-payment of fine",
+    "Limit to imprisonment for non-payment of fine, when imprisonment and fine awardable",
+    "Description of imprisonment for non-payment of fine",
+    "Imprisonment for non-payment of fine, when offence punishable with fine only",
+    "Imprisonment to terminate on payment of fine",
+    "Termination of imprisonment on payment of proportional part of fine",
+    "Fine leviable within six years, of during imprisonment. Death not to discharge property from liability",
+    "Limit of punishment of offence made up of several offences",
+    "Punishment of person guilty of one of several offences, the judgment stating that is doubtful of which",
+    "Solitary confinement",
+    "Limit of solitary confinement",
+    "Enhanced punishment for certain offences under Chapter XII or Chapter XVII after previous conviction",
+    " Act done by a person bound, or by mistake of fact believing himself bound, by law",
+
+    "Act done pursuant to the judgment or order of Court",
+    "Act done by a person justified, or by mistake of fact believing himself justified, by law",
+    "Accident in doing a lawful act",
+    "Act likely to cause harm, but done without criminal intent, and to prevent other harm",
+
+
+    "Act of a child under seven years of age",
+    "Act of a child above seven and under twelve of immature understanding",
+    "Act of a person of unsound mind",
+    "Act of a person incapable of judgment by reason of intoxication caused against his will",
+    "Offence requiring a particular intent or knowledge committed by one who is intoxicated",
+    "Act not intended and not known to be likely to cause death or grievous hurt, done by consent",
+    "Act not intended to cause death, done by consent in good faith for person's benefit",
+    "Act done in good faith for benefit of child or insane person, by or by consent of guardian",
+    "Provisos",
+    "Consent known to be given under fear or misconception",
+    "Consent of insane person",
+    "Consent of child",
+    "Exclusion of acts which are offences independently of harm caused",
+    "Act done in good faith for benefit of a person without consent",
+    "Provisos",
+    "Communication made in good faith",
+    "Act to which a person is compelled by threats",
+    "Act causing slight harm",
+    "Things done in private defence"
+    "Right of private defence of the body and of property"
+    "Right of private defence against the act of a person of unsound mind, etc."
+    "Acts against which there is no right of private defence"
+    "When the right of private defence of the body extends to causing death"
+    "When such right extends to causing any harm other than death"
+    "Commencement and continuance of the right of private defence of the body"
+    "When the right of private defence of property extends to causing death"
+    "When such right extends to causing any harm other than death"
+    "Commencement and continuance of the right of private defence of property"
+    "Right of private defence against deadly assault when there is a risk of harm to an innocent person"
+    "Abetment of a thing"
+    "Abettor"
+    "Abetment in India of offences outside India"
+    "Punishment of abetment if the act abetted is committed in consequence and when no express provision is made for its punishment"
+    "Punishment of abetment if the person abetted does an act with a different intention from that of the abettor"
+     "Liability of abettor when one act abetted and a different act done"
+     "Abettor when liable to cumulative punishment for act abetted and for act done"
+     "Liability of abettor for an effect caused by the act abetted different from that intended by the abettor"
+     "Abettor present when the offence is committed"
+     "Abetment of an offence punishable with death or imprisonment for life— if the offence not committed, if the act causing harm be done in consequence"
+     "Abetment of an offence punishable with imprisonment— if the offence be not committed, if the abettor or person abetted be a public servant whose duty it is to prevent the offence"
+     "Abetting commission of offence by the public or by more than ten persons"
+     "Concealing design to commit an offence punishable with death or imprisonment for life, if the offence be committed; if the offence be not committed"
+      "Public servant concealing the design to commit an offence which it is his duty to prevent, if the offence be committed; if the offence be punishable with death, etc., if the offence be not committed"
+     "Concealing design to commit an offence punishable with imprisonment, if the offence be committed; if the offence be not committed"
+     "Definition of criminal conspiracy"
+     "Punishment of criminal conspiracy"
+     "Waging, or attempting to wage war, or abetting waging of war, against the Government of India"
+     "Conspiracy to commit offences punishable by section 121"
+     "Collecting arms, etc., with the intention of waging war against the Government of India"
+     "Concealing with intent to facilitate the design to wage war"
+     "Assaulting President, Governor, etc., with intent to compel or restrain the exercise of any lawful power"
+     "Sedition"
+    "Waging war against any Asiatic power in alliance with the Government of India"
+    "Committing depredation on territories of power at peace with the Government of India"
+    "Receiving property taken by war or depredation mentioned in sections 125 and 126"
+    "Public servant voluntarily allowing a prisoner of State or war to escape"
+    "Public servant negligently suffering such a prisoner to escape"
+    "Aiding escape of, rescuing or harbouring such a prisoner"
+    "Abetting mutiny, or attempting to seduce a soldier, sailor or airman from his duty"
+    "Abetment of mutiny, if mutiny is committed in consequence thereof"
+    "Abetment of assault by soldier, sailor or airman on his superior officer, when in execution of his office"
+    "Abetment of such an assault, if the assault is committed"
+    "Abetment of desertion of soldier, sailor or airman"
+    "Harbouring deserter"
+    "Deserter concealed on board a merchant vessel through negligence of the master"
+    "Abetment of an act of insubordination by soldier, sailor or airman"
+    "Persons subject to certain Acts"
+     "Wearing garb or carrying token used by a soldier, sailor or airman"
+     "Unlawful assembly"
+    "Being a member of an unlawful assembly"
+    "Punishment"
+    "Joining an unlawful assembly armed with a deadly weapon"
+    "Joining or continuing in an unlawful assembly, knowing it has been commanded to disperse"
+    "Rioting"
+    "Rioting, armed with deadly weapon"
+    "Every member of unlawful assembly guilty of offence committed in prosecution of common object"
+    "Hiring, or conniving at hiring, of persons to join unlawful assembly"
+    "Knowingly joining or continuing in assembly of five or more persons after it has been commanded to disperse"
+    "Assaulting or obstructing public servant when suppressing riot, etc"
+    "Wantonly giving provocation, with intent to cause riot— if rioting be committed; if not committed"
+    "Promoting enmity between different groups on grounds of religion, race, place of birth, residence, language, etc., and doing acts prejudicial to maintenance of harmony"
+    "Offence committed in place of worship, etc"
+    "Punishment for knowingly carrying arms in any procession or organizing, or holding or taking part in any mass drill or mass training with arms"
+    "Imputation, assertions prejudicial to national-integration"
+    "Owner or occupier of land on which an unlawful assembly is held"
+    "Liability of person for whose benefit riot is committed"
+    "Liability of agent of owner or occupier for whose benefit riot is committed"
+    "Harbouring persons hired for an unlawful assembly"
+    "Being hired to take part in an unlawful assembly or riot; or to go armed"
+    "Affray"
+    "Punishment for committing affray"
+    "Public servant disobeying law, with intent to cause injury to any person"
+    "Public servant disobeying direction under law"
+    "Punishment for non-treatment of victim"
+    "Public servant framing an incorrect document with intent to cause injury"
+    "Public servant unlawfully engaging in trade"
+    "Public servant unlawfully buying or bidding for property"
+    "Personating a public servant"
+    "Wearing garb or carrying token used by public servant with fraudulent intent"
+    "Candidate, Electoral right defined"
+    "Bribery"
+    "Undue influence at elections"
+    "Personation at elections"
+    "Punishment for bribery"
+    "Punishment for undue influence or personation at an election"
+    "False statement in connection with an election"
+    "Illegal payments in connection with an election"
+    "Failure to keep election accounts"
+    "Absconding to avoid service of summons of other proceeding"
+    "Preventing service of summons or other proceeding, or preventing publication thereof"
+    "Non-attendance in obedience to an order from public servant"
+    "Non-appearance in response to a proclamation under section 82 of Act 2 of 1974"
+    "Omission to produce document to public servant by person legally bound to produce it"
+    "Omission to give notice or information to public servant by person legally bound to give it"
+    "Furnishing false information"
+    "Refusing oath or affirmation when duly required by public servant to make it"
+    "Refusing to answer public servant authorized to question"
+    "Refusing to sign statement"
+    "False statement on oath or affirmation to public servant or person authorized to administer an oath or affirmation"
+    "False information, with intent to cause public servant to use his lawful power to the injury of another person"
+    "Resistance to the taking of property by the lawful authority of a public servant"
+    "Obstructing sale of property offered for sale by authority of public servant"
+    "Illegal purchase or bid for property offered for sale by authority of public servant"
+    "Obstructing public servant in discharge of public functions"
+    "Omission to assist public servant when bound by law to give assistance"
+    "Disobedience to order duly promulgated by public servant"
+    "Threat of injury to public servant"
+    "Threat of injury to induce a person to refrain from applying for protection to public servant"
+    "Giving false evidence"
+    "Fabricating false evidence"
+    "Punishment for false evidence"
+    "Giving or fabricating false evidence with intent to procure conviction of capital offence if innocent person be thereby convicted and executed"
+    "Giving or fabricating false evidence with intent to procure conviction of offence punishable with imprisonment for life or imprisonment"
+    "Threatening any person to give false evidence"
+    "Using evidence known to be false"
+    "Issuing or signing false certificate"
+    "Using as true a certificate known to be false"
+    "False statement made in a declaration which is by law receivable as evidence"
+    "Using as true such declaration knowing it to be false"
+    "Causing the disappearance of evidence of an offence, or giving false information, to screen offender if a capital offence if punishable with imprisonment for life if punishable with less than ten years' imprisonment"
+    "Intentional omission to give information of an offence by a person bound to inform"
+    "Giving false information respecting an offence committed"
+    "Destruction of document to prevent its production as evidence"
+    "False personation for the purpose of an act or proceeding in suit or prosecution"
+    "Fraudulent removal or concealment of property to prevent its seizure as forfeited or in execution"
+    "Fraudulent claim to property to prevent its seizure as forfeited or in execution"
+    "Fraudulently suffering a decree for a sum not due"
+    "Dishonestly making a false claim in Court"
+    "Fraudulently obtaining a decree for a sum not due"
+    "False charge of an offence made with intent to injure"
+    "Harbouring offender if a capital offence if punishable with imprisonment for life or with imprisonment"
+    "Taking a gift, etc., to screen an offender from punishment if a capital offence if punishable with imprisonment for life or with imprisonment"
+    "Offering a gift or restoration of property in consideration of screening an offender if a capital offence if punishable with imprisonment for life or with imprisonment"
+    "Taking a gift to help to recover stolen property, etc"
+    "Harbouring an offender who has escaped from custody or whose apprehension has been ordered if a capital offence if punishable with imprisonment for life or with imprisonment"
+    "Penalty for harbouring robbers or dacoits"
+    "Public servant disobeying direction of law with intent to save a person from punishment or property from forfeiture"
+    "Public servant framing incorrect record or writing with intent to save a person from punishment or property from forfeiture"
+    "Public servant in a judicial proceeding corruptly making a report, etc., contrary to law"
+    "Commitment for trial or confinement by a person having authority who knows that he is acting contrary to law"
+    "Intentional omission to apprehend on the part of a public servant bound to apprehend"
+    "Intentional omission to apprehend on the part of a public servant bound to apprehend a person under sentence or lawfully committed"
+    "Escape from confinement or custody negligently suffered by public servant"
+    "Resistance or obstruction by a person to his lawful apprehension"
+    "Resistance or obstruction to lawful apprehension of another person"
+    "Omission to apprehend, or sufferance of escape, on part of a public servant, in cases not otherwise provided for"
+    "Resistance or obstruction to lawful apprehension, or escape or rescue in cases not otherwise provided for"
+    "Repealed"
+    "Violation of condition of remission of punishment"
+    "Intentional insult or interruption to a public servant sitting in a judicial proceeding"
+    "Disclosure of identity of the victim of certain offences, etc"
+    "Personation of a juror or assessor"
+    "Failure by a person released on bail or bond"
+    "Coin defined"
+    "Indian coin"
+    "Counterfeiting coin"
+    "Counterfeiting Indian coin"
+    "Making or selling instrument for counterfeiting coin"
+    "Making or selling instrument for counterfeiting Indian coin"
+   "Possession of instrument or material for the purpose of using the same for counterfeiting coin if Indian coin"
+   "Abetting in India the counterfeiting out of India of coin"
+   "Import or export of counterfeit coin"
+   "Import or export of counterfeits of the Indian coin"
+   "Delivery of coin, possessed with knowledge that it is counterfeit"
+  "Delivery of Indian coin, possessed with knowledge that it is counterfeit"
+  "Delivery of coin as genuine, which, when first possessed, the deliverer did not know to be counterfeit"
+  "Possession of counterfeit coin by person who knew it to be counterfeit when he became possessed thereof"
+  "Possession of Indian coin by person who knew it to be counterfeit when he became possessed thereof"
+  "Person employed in mint causing coin to be of different weight or composition from that fixed by law"
+  "Unlawfully taking coining instrument from mint"
+  "Fraudulently or dishonestly diminishing weight or altering composition of coin"
+  "Fraudulently or dishonestly diminishing weight or altering composition of Indian coin"
+  "Altering appearance of coin with intent that it shall pass as coin of different description"
+  "Altering appearance of Indian coin with intent that it shall pass as coin of different description"
+  "Delivery of coin, possessed with knowledge that it is altered"
+  "Delivery of Indian coin, possessed with knowledge that it is altered"
+  "Possession of coin by person who knew it to be altered when he became possessed thereof"
+  "Possession of Indian coin by person who knew it to be altered when he became possessed thereof"
+  "Delivery of coin as genuine which, when first possessed, the deliverer did not know to be altered"
+  "Counterfeiting Government stamp"
+  "Having possession of instrument or material for counterfeiting Government stamp"
+  "Making or selling instrument for counterfeiting Government stamp"
+  "Sale of counterfeit Government stamp"
+  "Having possession of counterfeit Government stamp"
+  "Using as genuine a Government stamp known to be counterfeit"
+]
